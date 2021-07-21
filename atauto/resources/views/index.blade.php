d91ddcde5c67190df725e6ab7ee0f5a61227a96e
leongchengyang <leongchengyang@gmail.com>
2021-07-21 10:55:14 +0800

    21/7/2021

--- atauto/resources/views/index.blade.php
+++ atauto/resources/views/index.blade.php
@@ -106,7 +106,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-6 mb-4">
+        <div class="col-lg-6 col-xxl-5 offset-xxl-0 mb-4">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
                     <h6 class="text-primary fw-bold m-0">Projects</h6>
@@ -134,6 +134,8 @@
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="col">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
                     <h6 class="text-primary fw-bold m-0">Todo List</h6>
@@ -172,59 +174,7 @@
                 </ul>
             </div>
         </div>
-        <div class="col">
-            <div class="row">
-                <div class="col-lg-6 mb-4">
-                    <div class="card textwhite bg-primary text-white shadow">
-                        <div class="card-body">
-                            <p class="m-0">Primary</p>
-                            <p class="text-white-50 small m-0">#4e73df</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card textwhite bg-success text-white shadow">
-                        <div class="card-body">
-                            <p class="m-0">Success</p>
-                            <p class="text-white-50 small m-0">#1cc88a</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card textwhite bg-info text-white shadow">
-                        <div class="card-body">
-                            <p class="m-0">Info</p>
-                            <p class="text-white-50 small m-0">#36b9cc</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card textwhite bg-warning text-white shadow">
-                        <div class="card-body">
-                            <p class="m-0">Warning</p>
-                            <p class="text-white-50 small m-0">#f6c23e</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card textwhite bg-danger text-white shadow">
-                        <div class="card-body">
-                            <p class="m-0">Danger</p>
-                            <p class="text-white-50 small m-0">#e74a3b</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card textwhite bg-secondary text-white shadow">
-                    <div class="card-body">
-                        <p class="m-0">Secondary</p>
-                            <p class="text-white-50 small m-0">#858796</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    </div>                                        
 </div>
 <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     
