608e5af19f3ef3d6147e976830101da6dbb7f677
leowyongfoo <jacksonleow6@gmail.com>
2021-09-07 00:01:16 +0800

    update leow

--- atauto/resources/views/inventory/report.blade.php
+++ atauto/resources/views/inventory/report.blade.php
@@ -10,21 +10,21 @@
     <h1>AtAuto</h1>
     <p>Inventory report</p>
     <div>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                        <th>ID</th>
-                        <th>Product</th>
-                        <th>Description</th>
-                        <th>Quantity</th>
-                        <th>Category</th>
-                        <th>Price/Unit</th>
-                        <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($inventories as $inventory)
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Product</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th>Category</th>
+                    <th>Price/Unit</th>
+                    <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($inventories as $inventory)
                         <tr>
                         <th>{{$inventory->id}}</th>
                         <td>{{$inventory->productName}}</td>
@@ -34,10 +34,10 @@
                         <td>{{$inventory->pricePerUnit}}</td>
                         <td>{{$inventory->statusID}}</td>
                         </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
+    </div>
 </body>
 </html>
