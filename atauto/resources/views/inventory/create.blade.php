e7d16fdb1ffb467b55fcb26b8fb3d116bd57faa0
leadless <njcnjc1020@gmail.com>
2021-09-13 22:53:03 +0800

    update jc

--- atauto/resources/views/inventory/create.blade.php
+++ atauto/resources/views/inventory/create.blade.php
@@ -11,17 +11,17 @@
 
             <p>
                 <label for="name" class="label">Product Name:</label>
-                <input class="inputField form-control" type="text" name="name" id="name">
+                <input class="inputField form-control" type="text" name="name" id="name" required>
             </p>
 
             <p>
                 <label for="description" class="label">Description:</label>
-                <textarea name="description" id="description" class="inputField form-control" cols="30" rows="8"></textarea>
+                <textarea name="description" id="description" class="inputField form-control" cols="30" rows="8" required></textarea>
             </p>
 
             <p>
                 <label for="quantity" class="label">Quantity:</label>
-                <input class="inputField form-control" type="number" name="quantity" id="quantity">
+                <input class="inputField form-control" type="number" name="quantity" id="quantity" required>
             </p>
             
             <label for="category">Category:</label>
@@ -33,21 +33,21 @@
 
             <p>
                 <label for="priceperunit" class="label">Price per unit: RM</label>
-                <input class="inputField form-control" type="text" name="priceperunit" id="priceperunit">
+                <input class="inputField form-control" type="text" name="priceperunit" id="priceperunit" required>
             </p>
 
             <p>
                 <label for="retailPrice" class="label">Retail Price:</label>
-                <input class="inputField form-control" type="text" name="retailPrice" id="retailPrice">
+                <input class="inputField form-control" type="text" name="retailPrice" id="retailPrice" required>
             </p>
 
             <p>
                 <label for="image" class="label">Upload Image:</label><br>
-                <input class="inputField" type="file" name="product-image" placeholder="select image">
+                <input class="inputField" type="file" name="product-image" placeholder="select image" required>
             </p>
             
             <p class="hoverColor">
-                <input type="submit" name="insert" value="Insert" class="btn btnStyleLighter">
+                <input type="submit" name="insert" value="Insert" class="btn btnStyleLighter" required>
             </p>
         </form>
     </div>
